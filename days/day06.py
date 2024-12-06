3df36f3996d7653c7809160517fedcd19c09712f
vbbcla <83726864+VivianBombycilla@users.noreply.github.com>
2024-12-06 12:00:33 +0000

    day 6 optimized

--- days/day06.py
+++ days/day06.py
@@ -15,73 +15,52 @@ with open(Path(__file__).parent / "../inputs/day06.txt") as file:
 # #.........
 # ......#...""".splitlines()
 
-def guard_leaving(guard):
-    heading = guard[1]
-    position = guard[0]
-    if heading == "<":
-        return position[1] == 0
-    if heading == ">":
-        return position[1] == cols-1
-    if heading == "^":
-        return position[0] == 0
-    if heading == "v":
-        return position[0] == rows-1
+def is_OOB(position):
+    return not ((0 <= position[0] < rows) and (0 <= position[1] < cols))
 
-offsets = {"<":[ 0,-1],
-           "^":[-1, 0],
-           ">":[ 0, 1],
-           "v":[ 1, 0]}
-turn_right = {"<":"^",
-              "^":">",
-              ">":"v",
-              "v":"<"}
+offsets = {"<":( 0,-1),
+           "^":(-1, 0),
+           ">":( 0, 1),
+           "v":( 1, 0)}
 
+turn_right = {( 0,-1):(-1, 0),
+               (-1, 0):( 0, 1),
+               ( 0, 1):( 1, 0),
+               ( 1, 0):( 0,-1)}
 
+# guard = [(row,col),(headingrow,headingcol)]
 BORDER = 0
 CLEAR = 1
 OBSTACLE = 2
-def thing_in_front_of_guard(guard,obstacles):
-    if guard_leaving(guard):
+def thing_at_position(position):
+    # next_position = (guard[0][0] + guard[1][0],guard[0][1] + guard[1][1])
+    if is_OOB(position):
         return BORDER
-    heading = guard[1]
-    position = guard[0]
-    next_position = [sum(x) for x in zip(position, offsets[heading])]
-    if next_position in obstacles:
+    if position in obstacles:
         return OBSTACLE
     return CLEAR
 
-def does_guard_loop(starting_guard,obstacles):
-    guard = starting_guard.copy()
-    guard2 = starting_guard.copy()
-    for i in range(rows*cols*4):
-        next_thing = thing_in_front_of_guard(guard,obstacles)
-        if next_thing == BORDER: return False
-        if next_thing == CLEAR:
-            guard[0] = [guard[0][i] + offsets[guard[1]][i] for i in range(2)]
-        if next_thing == OBSTACLE:
-            guard[1] = turn_right[guard[1]]
-        
-        # Do guard2 twice
-        next_thing2 = thing_in_front_of_guard(guard2,obstacles)
-        if next_thing2 == BORDER: return False
-        if next_thing2 == CLEAR:
-            guard2[0] = [guard2[0][i] + offsets[guard2[1]][i] for i in range(2)]
-        if next_thing2 == OBSTACLE:
-            guard2[1] = turn_right[guard2[1]]
-        next_thing2 = thing_in_front_of_guard(guard2,obstacles)
-        if next_thing2 == BORDER: return False
-        if next_thing2 == CLEAR:
-            guard2[0] = [guard2[0][i] + offsets[guard2[1]][i] for i in range(2)]
-        if next_thing2 == OBSTACLE:
-            guard2[1] = turn_right[guard2[1]]
-        
-        if guard == guard2:
-            return True
+def do_guard_step(guard):
+    next_position = (guard[0][0] + guard[1][0],guard[0][1] + guard[1][1])
+    next_thing = thing_at_position(next_position)
+    if next_thing == BORDER: return False
+    elif next_thing == CLEAR:
+        guard[0] = next_position
+    elif next_thing == OBSTACLE:
+        guard[1] = turn_right[guard[1]]
     return True
 
-    
+def does_guard_loop(starting_guard):
+    guard = starting_guard.copy()
+    guard2 = starting_guard.copy()
+    while True:
+        if not do_guard_step(guard2): return False
+        if not do_guard_step(guard2): return False
+        do_guard_step(guard)
+        if guard == guard2:
+            return True
 
-obstacles = []
+obstacles = set()
 rows = len(lines)
 cols = len(lines[0])
 # Initial map parse
@@ -89,24 +68,17 @@ for row in range(rows):
     for col in range(cols):
         thing = lines[row][col]
         if thing == "#":
-            obstacles.append([row,col])
+            obstacles.add((row,col))
         elif thing in "^>v<":
-            starting_guard = [[row,col],thing]
+            starting_guard = [(row,col),offsets[thing]]
 
-# print(obstacles)
-visited = []
+visited = set()
 guard = starting_guard.copy()
 start = time.time()
 while True:
-    if guard[0] not in visited:
-        visited.append(guard[0])
-    next_thing = thing_in_front_of_guard(guard,obstacles)
-    if next_thing == BORDER: break
-    if next_thing == CLEAR:
-        guard[0] = [guard[0][i] + offsets[guard[1]][i] for i in range(2)]
-    if next_thing == OBSTACLE:
-        guard[1] = turn_right[guard[1]]
-        
+    visited.add(guard[0])
+    if not do_guard_step(guard):
+        break    
     # input(guard)
 end = time.time()
 result1 = len(visited)
@@ -115,14 +87,15 @@ print(result1,end-start)
 result2 = 0
 counter = 0
 
-for visit in visited[1:]:
-    start = time.time()
-    new_obstacles = obstacles.copy()
-    new_obstacles.append(visit)
-    if does_guard_loop(starting_guard,new_obstacles):
+start = time.time()
+visited.remove(starting_guard[0])
+for visit in visited:
+    obstacles.add(visit)
+    if does_guard_loop(starting_guard):
         result2 += 1
     counter += 1
-    end = time.time()
-    print(counter,end-start)
+    obstacles.remove(visit)
+    # print(counter,end-start)
+end = time.time()
 
-print(result2)
+print(result2,end-start)
